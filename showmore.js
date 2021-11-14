51a85204c10355a8f61c1d6debca3f37a8be4520
Chen ZengYao <isorandom95@gmail.com>
2021-11-14 15:18:17 +0800

    Update 4.49
    fixed showmore.js to single click

--- showmore.js
+++ showmore.js
@@ -1,14 +1,32 @@
 function showmore(text, arrow) {
     var moreText = document.getElementById(text);
     var arrowChange = document.getElementById(arrow);
- 
-    if (moreText.style.display != "none") 
-    {
-        arrowChange.style.transform = "rotate(-45deg)";
-        moreText.style.display = "none";
-    } 
-    else {
+
+
+    if(!moreText)return true;
+    if (getStyle(moreText, "display") == "none") 
+    {   
         arrowChange.style.transform = "rotate(45deg)";
         moreText.style.display = "inline";
+    } 
+    else {
+        arrowChange.style.transform = "rotate(-45deg)";
+        moreText.style.display = "none";
     }
+}
+
+function getStyle(el, name)
+{
+  // the way of the DOM
+  if ( document.defaultView && document.defaultView.getComputedStyle )
+  {
+    var style = document.defaultView.getComputedStyle(el, null);
+    if ( style )
+      return style[name];
+  }
+  // IE-specific
+  else if ( el.currentStyle )
+    return el.currentStyle[name];
+  
+  return null;
 }
